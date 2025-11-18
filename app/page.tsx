1d009fc40ca500c22befb8093979381a99051ea2
최정원 <choejeong-won@choejeong-won-ui-MacBookPro.local>
2025-11-18 20:18:53 +0900

    style: 모바일 배경 이미지 왼쪽 정렬

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
           src="/images/mainbackground.png"
           alt="PEIT 배경"
           fill
-          className="object-cover md:object-center"
+          className="object-cover object-left md:object-center"
           quality={90}
           sizes="100vw"
           priority
